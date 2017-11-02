decabf4c16188ceb8650b16b1623e87ad20c3af4
Arturas Molcanovas <a.molcanovas@gmail.com>
2017-11-02 18:50:23 +0000

    typings fixed

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,19 @@
-declare type Fill = 0 | 1 | 2 | 3 | 4;
+export declare type Fill = 0 | 1 | 2 | 3 | 4;
 
-declare interface IRect {
+export declare interface IRect {
   count: number,
   date: string,
   fill: Fill
 }
 
-declare type IG = IRect[];
+export declare type IG = IRect[];
 
-declare interface IMonth {
+export declare interface IMonth {
   txt: string,
   x: number
 }
 
-declare interface IParsedPayload {
+export declare interface IParsedPayload {
   months: IMonth[],
   gs: IG[]
 }
